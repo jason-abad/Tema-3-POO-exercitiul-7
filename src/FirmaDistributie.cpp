4bc2aee305a650c856fc6f9dbd3c939d8ad24316
jason-abad <58149957+jason-abad@users.noreply.github.com>
2020-01-04 19:29:35 +0200

    Update FirmaDistributie.cpp

--- src/FirmaDistributie.cpp
+++ src/FirmaDistributie.cpp
@@ -1,1 +1,3 @@
+Salut, cum ai pus elementele in vectorul produs? Cine e x? Unde l-ai citit? Poti sa-mi raspunzi pe facebook, as aprecia ajutorul :D
 
+https://www.facebook.com/ruxandracamelia.stanciu.7
